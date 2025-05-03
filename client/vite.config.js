7315f3ece6eef51561d6d7ec017068c63d097c3f
Martin Harley <mharley@phasemargin.com>
2025-05-02 18:15:33 -0700

    feat: added vite to cache dependency path.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/MessagesAPI/',
+  build: {
+    target: 'esnext',
+    minify: 'terser'
+  },
   server: {
     port: 3000,
     proxy: {
