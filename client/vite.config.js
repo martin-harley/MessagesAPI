f838f09fb3bb74e7ff92e64c7c04692f5cccbf5e
Martin <mharley007@gmail.com>
2025-05-03 16:25:46 -0700

    Updated index.html AND vite.config.js to use client/dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
   base: '/MessagesAPI/',
   build: {
     target: 'esnext',
-    minify: 'terser'
+    minify: 'terser',
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url))
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   server: {
     port: 3000,
