f414fa84266420c1446a3b923b41839759675937
Martin <mharley007@gmail.com>
2025-05-03 17:48:38 -0700

    Updated base url to /

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/MessagesAPI/',
+  base: '/',
   build: {
     target: 'esnext',
     minify: 'terser'
